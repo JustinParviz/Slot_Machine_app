a3cea6eb624e3c2a25b44f011174d8d831a0ef55
JustinParviz <jarshravan@gmail.com>
2025-04-17 13:07:30 -0700

    Used pseudo code to create a step by step outline for the project

--- project.js
+++ project.js
@@ -1,1 +1,14 @@
+// Slot Machine app
+
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings or take their bet if they lost
+// 7. Play again or handle situation if the user has no money left
+
+
+
+
 
