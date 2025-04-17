25b3b9160fb4342e4588a26b4d9947edf77e9200
JustinParviz <jarshravan@gmail.com>
2025-04-17 14:32:41 -0700

    Built out deposit function

---
+++ notes.js
@@ -0,0 +1,48 @@
+
+// function deposit() {
+// 
+// }
+
+// - The ES6 style of writing a function involves writing a variable:
+// - The example above works the exact same way as the example below
+
+// const deposit = () => {
+    
+// }
+
+
+
+// - The code below imports and allows you to use the package that was installed 
+// called Prompt Sync.
+
+// const prompt = require("prompt-sync")();
+
+// NOTE: The Prompt Sync package gets the user input.
+// - Adding another set of parenthesis at the end will require this package / module that 
+// you're using, which kind of imports it into the program, to give you access to a function 
+// that you can use to get the user input when you call it with these parenthesis.
+
+
+
+// NOTE: The parseFloat function is just going to take a string and it's going convert it into its 
+// floating point value.
+// - If you have a string that is not a number, a string like "hello", and you use the parseFloat 
+// function its going to give you something called NaN, which stands for Not a Number.
+// - If you get NaN, that means that the user didn't enter a valid deposit amount and you need to 
+// ask them to do that again. Or they can just quit the program and they would have to try again.
+// - isNaN is a function that checks if something is not a number.
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
--- project.js
+++ project.js
@@ -9,6 +9,20 @@
 // 7. Play again or handle situation if the user has no money left
 
 
+const prompt = require("prompt-sync")();
 
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
 
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount, please try again.");
+        } else {
+            return numberDepositAmount;
+        }
+    }
+};
 
+const depositAmount = deposit();
+console.log(depositAmount);
