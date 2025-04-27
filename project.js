3aeee6e5cbb57298426d0afe9ebf15ce1edc2d66
JustinParviz <jarshravan@gmail.com>
2025-04-27 15:53:06 -0700

    Put the entire game inside the game function

--- project.js
+++ project.js
@@ -141,14 +141,24 @@ const getWinnings = (rows, bet, lines) => {
 };
 
 
-let balance = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance, numberOfLines);
-const reels = spin();
-const rows = transpose(reels);
-printRows(rows);
-const winnings = getWinnings(rows, bet, numberOfLines);
-console.log("You won, $" + winnings.toString())
+const game = () => {
+    let balance = deposit();
+
+    while (true) {
+        const numberOfLines = getNumberOfLines();
+        const bet = getBet(balance, numberOfLines);
+        const reels = spin();
+        const rows = transpose(reels);
+        printRows(rows);
+        const winnings = getWinnings(rows, bet, numberOfLines);
+        console.log("You won, $" + winnings.toString())
+    }
+};
+
+
+game();
+
+
 
 
 
